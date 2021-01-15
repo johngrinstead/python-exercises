47b1bd8f07b62fab1bbe1942b347ea56a788e0e9
John Grinstead <john.allen.grinstead@gmail.com>
2021-01-15 16:57:57 -0600

    worked on import exercise

---
+++ import_exercises.py
@@ -0,0 +1,60 @@
+## Import and test 3 of the functions from your functions exercise file.
+#Import each function in a different way:
+# -import the module and refer to the function with the . syntax
+# -use from to import the function directly
+# -use from and give the function a different name
+
+import function_exercises
+
+function_exercises.apply_discount(89, 25)
+
+############################################
+
+import function_exercises as fe
+
+fe.handle_commas('123,123,123')
+
+######################################
+
+from function_exercises import get_letter_grade
+
+get_letter_grade(78)
+
+#################################################
+
+## 1. How many different ways can you combine the letters from "abc" with the numbers 1, 2, and 3?
+
+import itertools as it
+len(list(it.product([1, 2, 3], 'abc')))
+
+
+## 2. How many different ways can you combine two of the letters from "abcd"?
+
+len(list(it.combinations('abcd', 2)))
+
+#############################################
+#############################################
+
+# A. Total number of users
+
+len(profiles)
+
+# B. Number of active users
+
+count = 0 
+
+for user in profiles:
+    if user['isActive'] == True:
+        count = count + 1
+print(count)
+
+# C. Number of inactive users
+
+count = 0 
+
+for user in profiles:
+    if user['isActive'] == False:
+        count = count + 1
+print(count)
+
+
